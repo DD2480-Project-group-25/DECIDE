009693ccdf1b923fe00dea7a83f25756a371652f
Jesper Larsson <jesper.a.larsson@outlook.com>
2019-01-23 14:11:44 +0100

    adds basic java class to test out setup

---
+++ src/main/java/MyTest.java
@@ -0,0 +1,13 @@
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println(messageProvider());
+    }
+
+    /**
+     * messageProivider provides a hello world message.
+     * @return a hello world message
+     */
+    static String messageProvider() {
+        return "Hello world!";
+    }
+}
---
+++ src/test/java/TestMyTest.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestMyTest {
+    @Test
+    public void testMessageProvider() {
+        String expected = "Hello world!";
+        String actual = MyTest.messageProvider();
+        Assert.assertEquals(expected, actual);
+    }
+}

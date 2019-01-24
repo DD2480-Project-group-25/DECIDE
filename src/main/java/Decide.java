ca67323377d8b30251d8da6e3947e8410ce17d68
Marcus Granström <marcus.granstroem@gmail.com>
2019-01-24 11:46:40 +0100

    Fix #23 - Adds Decide class to project (#24)
    * adds Decide class to project
    
    Empty class in order to start implementing all the LICs
    
    * change method name to follow convention
    
    Changes method name from Decide to decideLaunch to avoid
    missunderstandings

---
+++ src/main/java/Decide.java
@@ -0,0 +1,22 @@
+/**
+ * DECIDE - Launch Interceptor Program
+ * #TODO
+ *
+ * @author  Alzahraa Salman, Helena Alinder, Jesper Larsson, Marcus Granström, Veronica Hage
+ * @version 0.1
+ * @since   0.1
+ */
+public class Decide {
+
+    /**
+     * Decides if the "launch-unlock" signal will be generated.
+     * The launch decision is made from the Final Unlocking Vector (FUV), where
+     * all values needs to be true in order to unlock the launch signal.
+     *
+     * @return true iff ALL values in the FUV is true, otherwise the return
+     * value is set to false
+     */
+    boolean decideLaunch() {
+        return false;
+    }
+}

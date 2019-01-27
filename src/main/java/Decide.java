86181daf7dd374484797b8984170e22819f5a9ce
--add <--add>
2019-01-27 19:51:00 +0100

    Orders LIC methods from 0 to 14

--- src/main/java/Decide.java
+++ src/main/java/Decide.java
@@ -44,105 +44,6 @@ public class Decide {
         return false;
     }
 
-    public static boolean LIC8(Point[] pts, Parameters params) {
-        int a_pts = params.a_pts;
-        int b_pts = params.b_pts;
-        double radius1 = params.radius1;
-
-        if(a_pts < 1) {
-            throw new IllegalArgumentException("A_PTS must be greater or equal to 1");
-        }
-        if(b_pts < 1) {
-            throw new IllegalArgumentException("B_PTS must be greater or equal to 1");
-        }
-        if(pts.length < 5) {
-            return false;
-        }
-        if(a_pts + b_pts > pts.length - 3) {
-            throw new IllegalArgumentException("A_PTS + B_PTS not less or equal to NUMPOINTS -3");
-        }
-
-        for(int i = 0; i < pts.length - a_pts - b_pts - 2; i++) {
-            Point a = pts[i];
-            Point b = pts[i + a_pts + 1];
-            Point c = pts[i + a_pts + 1 + b_pts + 1];
-
-            if(!Circle.fits(radius1, a, b, c)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    /**
-     * Checks that there exists at least one set of three data points separated
-     * by exactly c_pts and d_pts consecutive intervening points, respectively,
-     * that form an angle such that: angle < (PI − epsilon) or angle > (PI + epsilon)
-     * @param pts array of Point objects
-     * @param params Parameter object
-     * @return true if the condition, above, for the angle is met
-     */
-    public static boolean LIC9(Point[] pts, Parameters params) {
-        int c_pts = params.c_pts;
-        int d_pts = params.d_pts;
-        if (pts.length < 5) {
-            return false;
-        }
-        if (c_pts < 1 || d_pts < 1){
-            throw new IllegalArgumentException("c_pts and d_pts must be greater or equal to 1");
-        }
-        for (int i = 0; i < pts.length - c_pts - d_pts - 2; i++) {
-            Point x = pts[i];
-            Point v = pts[i + c_pts + 1];
-            Point y = pts[i + c_pts + d_pts + 2];
-            //check if x and v or y and v coincide
-            if (x.X == v.X && x.Y == v.Y || y.X == v.X && y.Y == v.Y) {
-                continue;
-            }
-            //calculate angle using law of cosine
-            double angle = Math.acos((Math.pow(x.distance(v),2) + Math.pow(y.distance(v),2) - Math.pow(x.distance(y),2))
-                /(2 * x.distance(v) * y.distance(v)));
-            if (angle < (Math.PI - params.epsilon) || angle > (Math.PI + params.epsilon)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * Checks that there are three consecutive points, separated by e_pts and f_pts points respectively,
-     * that forms a triangle with area greater than params.area1
-     * @param pts array of Point objects
-     * @param params Parameter object
-     * @return true if the condition above is met
-     */
-    public static boolean LIC10(Point[] pts, Parameters params) {
-        if (pts.length < 5) {
-            return false;
-        }
-        int e_pts = params.e_pts;
-        int f_pts = params.f_pts;
-        if (e_pts < 1 || f_pts < 1) {
-            throw new IllegalArgumentException("e_pts and f_pts must be greater than or equals to 1");
-        }
-        if ((e_pts + f_pts) > pts.length - 3) {
-            throw new IllegalArgumentException("Condition (e_pts + f_pts) <= (numpoints - 3) is not met.");
-        }
-        Point a, b, c;
-        double triangleArea;
-        for (int i = 0; i < pts.length - e_pts - f_pts - 2; i++) {
-            a = pts[i];
-            b = pts[i + e_pts + 1];
-            c = pts[i + e_pts + f_pts + 2];
-            triangleArea = Point.triangleArea(a, b, c);
-            if (triangleArea > params.area1) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     /**
      * Checks if there are three consecutive data points that do not all fit into a circle of radius1.
      * @param pts array of Point objects
@@ -272,50 +173,6 @@ public class Decide {
         return false;
     }
 
-
-    /**
-     * Checks that there exists at least one set of three data points, separated by exactly e_pts
-     * and f_pts consecutive points, respectively, that are the vertices of a triangle
-     * with area greater than AREA1. 
-     * In addition, there exist three data points (can be the same as the mentioned points) separated by 
-     * exactly e_pts and f_pts consecutive  points, respectively, that are the vertices of
-     * a triangle with area less than AREA2. 
-     * @param pts array of Points objects
-     * @param params Parameter object
-     * @return true if both conditions above are true 
-     */
-    public static boolean LIC14(Point[] pts, Parameters params) {
-        int e_pts = params.e_pts;
-        int f_pts = params.f_pts;
-        boolean greaterThanArea1 = false;
-        boolean lesserThanArea2 = false;
-
-        if (pts.length < 5) {
-            return false;
-        }
-        if(params.area1 < 0 || params.area2 < 0) {
-            throw new IllegalArgumentException("AREA1 and AREA2 must be greater than or equal to 0");
-        }
-
-        for (int i = 0; i < pts.length - e_pts - f_pts - 2; i++) {
-            Point x = pts[i];
-            Point v = pts[i + e_pts + 1];
-            Point y = pts[i + e_pts + f_pts + 2];
-            //calculate area (According to https://www.mathopenref.com/coordtrianglearea.html) and set greater and lesser
-            double area = Point.triangleArea(x, v, y);
-            if (area > params.area1) {
-                greaterThanArea1 = true;
-            }
-            if (area < params.area2) {
-                lesserThanArea2 = true;
-            }
-            if (greaterThanArea1 && lesserThanArea2) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     /**
      * Check difference between two points' X-coordinates.
      * @param pts array of Point objects
@@ -330,65 +187,6 @@ public class Decide {
             b = pts[i + 1];
             if ((b.X - a.X) < 0) {
                 return true;
-            }
-        }
-        return false;
-    }
-    
-     /*
-     * Checks if there is two points, separated by K_pts consecutive intervening
-     * points, with a distance greater than LENGTH1 between each other.
-     *
-     * @param pts array of data points of type Point
-     * @param params Parameter object acting as configuration
-     * @return boolean which is true iff LIC7 condition is met
-     */
-    public static boolean LIC7(Point[] pts, Parameters params) {
-        // NUMPOINTS needs to be at least 3
-        if (pts.length < 3) {
-            return false;
-        }
-
-        if (1 <= params.k_pts && params.k_pts <= pts.length - 2) { /* Condition fulfilled */
-        } else {
-            throw new IllegalArgumentException("K_pts needs to be greater or equal" +
-                    " to 1 and lower or equal to amount of data points - 2. Check LIC7");
-        }
-
-        // Check distance between point p[i] and p[i + k_pts + 1]
-        for (int i = 0; i < pts.length - (params.k_pts + 1); i++) {
-            double dist = pts[i].distance(pts[i + params.k_pts + 1]);
-
-            if (dist > params.length1) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * LIC11 is true if there exists at least one set of two data points, separated by exactly g_pts
-     * consecutive intervening points, such that the first data point is to the right of the second point
-     * on the x-axis.
-     * @param pts array of Points objects
-     * @param params Parameter object
-     * @return true if LIC3 as described above is true, false otherwise.
-     */
-    public static boolean LIC11(Point[] pts, Parameters params) {
-        if (params.g_pts < 1)
-            throw new IllegalArgumentException("g_pts must be greater than or equal to 1");
-        else if (pts.length < 3) {
-            return false;
-        }
-        Point p1, p2;
-
-        for (int i = 0; i < pts.length - params.g_pts - 1; i++) {
-            p1 = pts[i];
-            p2 = pts[i + params.g_pts + 1];
-            for (int j = 0; j < params.g_pts; j++) {
-                if (p2.X - p1.X < 0) {
-                    return true;
-                }
             }
         }
         return false;
@@ -460,8 +258,166 @@ public class Decide {
         }
         return false;
      }
-  
-     /*
+
+    /**
+     * Checks if there is two points, separated by K_pts consecutive intervening
+     * points, with a distance greater than LENGTH1 between each other.
+     *
+     * @param pts array of data points of type Point
+     * @param params Parameter object acting as configuration
+     * @return boolean which is true iff LIC7 condition is met
+     */
+    public static boolean LIC7(Point[] pts, Parameters params) {
+        // NUMPOINTS needs to be at least 3
+        if (pts.length < 3) {
+            return false;
+        }
+
+        if (1 <= params.k_pts && params.k_pts <= pts.length - 2) { /* Condition fulfilled */
+        } else {
+            throw new IllegalArgumentException("K_pts needs to be greater or equal" +
+                    " to 1 and lower or equal to amount of data points - 2. Check LIC7");
+        }
+
+        // Check distance between point p[i] and p[i + k_pts + 1]
+        for (int i = 0; i < pts.length - (params.k_pts + 1); i++) {
+            double dist = pts[i].distance(pts[i + params.k_pts + 1]);
+
+            if (dist > params.length1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean LIC8(Point[] pts, Parameters params) {
+        int a_pts = params.a_pts;
+        int b_pts = params.b_pts;
+        double radius1 = params.radius1;
+
+        if(a_pts < 1) {
+            throw new IllegalArgumentException("A_PTS must be greater or equal to 1");
+        }
+        if(b_pts < 1) {
+            throw new IllegalArgumentException("B_PTS must be greater or equal to 1");
+        }
+        if(pts.length < 5) {
+            return false;
+        }
+        if(a_pts + b_pts > pts.length - 3) {
+            throw new IllegalArgumentException("A_PTS + B_PTS not less or equal to NUMPOINTS -3");
+        }
+
+        for(int i = 0; i < pts.length - a_pts - b_pts - 2; i++) {
+            Point a = pts[i];
+            Point b = pts[i + a_pts + 1];
+            Point c = pts[i + a_pts + 1 + b_pts + 1];
+
+            if(!Circle.fits(radius1, a, b, c)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Checks that there exists at least one set of three data points separated
+     * by exactly c_pts and d_pts consecutive intervening points, respectively,
+     * that form an angle such that: angle < (PI − epsilon) or angle > (PI + epsilon)
+     * @param pts array of Point objects
+     * @param params Parameter object
+     * @return true if the condition, above, for the angle is met
+     */
+    public static boolean LIC9(Point[] pts, Parameters params) {
+        int c_pts = params.c_pts;
+        int d_pts = params.d_pts;
+        if (pts.length < 5) {
+            return false;
+        }
+        if (c_pts < 1 || d_pts < 1){
+            throw new IllegalArgumentException("c_pts and d_pts must be greater or equal to 1");
+        }
+        for (int i = 0; i < pts.length - c_pts - d_pts - 2; i++) {
+            Point x = pts[i];
+            Point v = pts[i + c_pts + 1];
+            Point y = pts[i + c_pts + d_pts + 2];
+            //check if x and v or y and v coincide
+            if (x.X == v.X && x.Y == v.Y || y.X == v.X && y.Y == v.Y) {
+                continue;
+            }
+            //calculate angle using law of cosine
+            double angle = Math.acos((Math.pow(x.distance(v),2) + Math.pow(y.distance(v),2) - Math.pow(x.distance(y),2))
+                    /(2 * x.distance(v) * y.distance(v)));
+            if (angle < (Math.PI - params.epsilon) || angle > (Math.PI + params.epsilon)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Checks that there are three consecutive points, separated by e_pts and f_pts points respectively,
+     * that forms a triangle with area greater than params.area1
+     * @param pts array of Point objects
+     * @param params Parameter object
+     * @return true if the condition above is met
+     */
+    public static boolean LIC10(Point[] pts, Parameters params) {
+        if (pts.length < 5) {
+            return false;
+        }
+        int e_pts = params.e_pts;
+        int f_pts = params.f_pts;
+        if (e_pts < 1 || f_pts < 1) {
+            throw new IllegalArgumentException("e_pts and f_pts must be greater than or equals to 1");
+        }
+        if ((e_pts + f_pts) > pts.length - 3) {
+            throw new IllegalArgumentException("Condition (e_pts + f_pts) <= (numpoints - 3) is not met.");
+        }
+        Point a, b, c;
+        double triangleArea;
+        for (int i = 0; i < pts.length - e_pts - f_pts - 2; i++) {
+            a = pts[i];
+            b = pts[i + e_pts + 1];
+            c = pts[i + e_pts + f_pts + 2];
+            triangleArea = Point.triangleArea(a, b, c);
+            if (triangleArea > params.area1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * LIC11 is true if there exists at least one set of two data points, separated by exactly g_pts
+     * consecutive intervening points, such that the first data point is to the right of the second point
+     * on the x-axis.
+     * @param pts array of Points objects
+     * @param params Parameter object
+     * @return true if LIC3 as described above is true, false otherwise.
+     */
+    public static boolean LIC11(Point[] pts, Parameters params) {
+        if (params.g_pts < 1)
+            throw new IllegalArgumentException("g_pts must be greater than or equal to 1");
+        else if (pts.length < 3) {
+            return false;
+        }
+        Point p1, p2;
+
+        for (int i = 0; i < pts.length - params.g_pts - 1; i++) {
+            p1 = pts[i];
+            p2 = pts[i + params.g_pts + 1];
+            for (int j = 0; j < params.g_pts; j++) {
+                if (p2.X - p1.X < 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
      * Checks if there is two points, separated by K_pts consecutive intervening
      * points, with a distance greater than LENGTH1 between each other. If such
      * points exists, check if there is (at least) two points, separated by K_pts
@@ -500,7 +456,7 @@ public class Decide {
                 return true;
             }
         }
-      
+
         return false;
     }
 
@@ -555,5 +511,47 @@ public class Decide {
       }
 
       return false;
-  }
+    }
+    /**
+     * Checks that there exists at least one set of three data points, separated by exactly e_pts
+     * and f_pts consecutive points, respectively, that are the vertices of a triangle
+     * with area greater than AREA1.
+     * In addition, there exist three data points (can be the same as the mentioned points) separated by
+     * exactly e_pts and f_pts consecutive  points, respectively, that are the vertices of
+     * a triangle with area less than AREA2.
+     * @param pts array of Points objects
+     * @param params Parameter object
+     * @return true if both conditions above are true
+     */
+    public static boolean LIC14(Point[] pts, Parameters params) {
+        int e_pts = params.e_pts;
+        int f_pts = params.f_pts;
+        boolean greaterThanArea1 = false;
+        boolean lesserThanArea2 = false;
+
+        if (pts.length < 5) {
+            return false;
+        }
+        if(params.area1 < 0 || params.area2 < 0) {
+            throw new IllegalArgumentException("AREA1 and AREA2 must be greater than or equal to 0");
+        }
+
+        for (int i = 0; i < pts.length - e_pts - f_pts - 2; i++) {
+            Point x = pts[i];
+            Point v = pts[i + e_pts + 1];
+            Point y = pts[i + e_pts + f_pts + 2];
+            //calculate area (According to https://www.mathopenref.com/coordtrianglearea.html) and set greater and lesser
+            double area = Point.triangleArea(x, v, y);
+            if (area > params.area1) {
+                greaterThanArea1 = true;
+            }
+            if (area < params.area2) {
+                lesserThanArea2 = true;
+            }
+            if (greaterThanArea1 && lesserThanArea2) {
+                return true;
+            }
+        }
+        return false;
+    }
 }

7e412e7692582d1817d83cc0f83aafa934443da6
alzahraasalman <alzahraa@kth.se>
2019-01-28 13:46:17 +0100

    Refactors comments on TestLIC14 #81

--- src/test/java/TestLIC14.java
+++ src/test/java/TestLIC14.java
@@ -3,8 +3,8 @@ import org.junit.Test;
 
 public class TestLIC14 {
   /**
-   * Same set of three consecutive points satisfies the conditions e_pts=1, d_pts=1, Area1=20,
-   * Area2=30, (area=28)
+   * Test that LIC14 method works. Same set of three consecutive data points (a,c,e) satisfies the
+   * conditions. e_pts=1, d_pts=1, Area1=20, Area2=30, (area=28)
    */
   @Test
   public void testSameSet() {
@@ -20,8 +20,9 @@ public class TestLIC14 {
   }
 
   /**
-   * Different sets of three consecutive points satisfies the conditions e_pts=1, d_pts=2, Area1=20,
-   * Area2=22, area of triangle acf = 28 and area of triangle bdg = 21
+   * Test that LIC14 method works. Different sets of three consecutive points satisfies the
+   * conditions (a,c,f and b,d,g) e_pts=1, d_pts=2, Area1=20, Area2=22 Area of triangle acf = 28 &
+   * area of triangle bdg = 21
    */
   @Test
   public void testDifferentSet() {
@@ -40,8 +41,8 @@ public class TestLIC14 {
   }
 
   /**
-   * Only one condition is met, area is not lesser than AREA2 e_pts=1, d_pts=1, Area1=20, Area2=27,
-   * (area=28)
+   * Test that LIC14 returns false when ONLY one condition is met. Only one condition is met, area
+   * is not lesser than AREA2 e_pts=1, d_pts=1, Area1=20, Area2=27, (area=28)
    */
   @Test
   public void testOneConditionIsMet() {
@@ -56,7 +57,7 @@ public class TestLIC14 {
     Assert.assertFalse(Decide.LIC14(pts, params));
   }
 
-  /** Non of the conditions are met e_pts=1, d_pts=1, Area1=29, Area2=27, (area=28) */
+  /** Test that LIC14 returns false when none of the conditions are met. */
   @Test
   public void testNoConditionIsMet() {
     Parameters params = new Parameters(0, 0, 0, 0, 0, 29, 27, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0);
@@ -70,6 +71,7 @@ public class TestLIC14 {
     Assert.assertFalse(Decide.LIC14(pts, params));
   }
 
+  /** Check that IllegalArgumentException is thrown when area is negative */
   @Test(expected = IllegalArgumentException.class)
   public void testNegativeArea() {
     Parameters params = new Parameters(0, 0, 0, 0, 0, -1, 22, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0);
@@ -83,6 +85,7 @@ public class TestLIC14 {
     Decide.LIC14(pts, params);
   }
 
+  /** Test that LIC14 method returns false when number of points is less than 5. */
   @Test
   public void testLessThanFivePoints() {
     Parameters params = new Parameters(0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0);

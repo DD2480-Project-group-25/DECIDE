8c18d743f84685fdfb9e2d59699d6fa93a8121fb
Alzahraa <alzahraa@kth.se>
2019-01-28 13:09:09 +0100

    Refactors comments on TestLIC4

--- src/test/java/TestLIC4.java
+++ src/test/java/TestLIC4.java
@@ -2,7 +2,11 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TestLIC4 {
-
+  /**
+  * Test that LIC4 method returns true for the given data points,
+  * when q_pts=3 and quads=2. 
+  * There exists at least one set of 3 data points that lie in more than 2 quadrants  
+  */
   @Test
   public void testPositive() {
     Parameters params = new Parameters(0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0);
@@ -16,6 +20,11 @@ public class TestLIC4 {
     Assert.assertEquals(true, Decide.LIC4(pts, params));
   }
 
+  /**
+  * Test that LIC4 method returns false for the given data points,
+  * when q_pts=2 and quads=2. 
+  * There should exist at least one set of 2 data points that lie in MORE than 2 quadrants  
+  */
   @Test
   public void testNegative() {
     Parameters params = new Parameters(0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0);
@@ -27,6 +36,9 @@ public class TestLIC4 {
     Assert.assertEquals(false, Decide.LIC4(pts, params));
   }
 
+  /**
+  * Test that LIC4 method works for data points laying on the axises 
+  */
   @Test
   public void testOnAxis() {
     Parameters params = new Parameters(0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0);
@@ -37,7 +49,11 @@ public class TestLIC4 {
     Point[] pts = {a, b, c};
     Assert.assertEquals(true, Decide.LIC4(pts, params));
   }
-
+  /**
+  * Check that IllegalArgumentException is thrown when 
+  * q_pts is not between 2 and pts.length or when
+  * quads is not between 1 and 3.
+  */
   @Test
   public void testIllegalArgument() {
     Point a = new Point(1, 1);
